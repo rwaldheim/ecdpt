a0e35a8c81098fbc0697c4bd8032b7c866580050
Ryan Waldheim <ryan@waldhe.im>
2020-03-30 16:16:49 -0400

    made each graph generation optional

--- arbinimport.R
+++ arbinimport.R
@@ -57,7 +57,8 @@ if (interactive()) {
       ),
       
       fluidRow(
-        radioButtons("gGraphs", "Generate Graphs?", choices = c("Yes (may take longer)" = "genGraphs", "No" = "noGenGraphs"), inline = TRUE)
+        checkboxGroupInput("gGraphs", "Generate Graphs?", choices = c("dQdV Graphs", "Voltage Profiles", "Voltage vs. Time", "Discharge Capacity", "Discharge Areal Capacity",
+                                                                      "Total Discharge Capacity", "Average Voltage"), inline = TRUE)
       ),
       
       fluidRow(
@@ -129,7 +130,7 @@ if (interactive()) {
         shinyReturn <- ""
         shinyalert("Uh Oh!", "You do not appear to be in a shared directory!", type = "error",
              showConfirmButton = TRUE, confirmButtonText = "Ignore", showCancelButton = TRUE,
-             cancelButtonText = "Cancel",
+             cancelButtonText = "Abort",
              callbackR = function(x) {
                if (x) {
                  runscript()
@@ -172,14 +173,10 @@ if (interactive()) {
           tmp_excel <- read_excel(toString(data$datapath[row]), toString(data$sheet[row]))
           
           dir.create(paste(input$dirLocation, data$sheet[row], sep = "/"))
-          if (input$gGraphs == "genGraphs") {
-            dir.create(paste(input$dirLocation, data$sheet[row], "dQdV Plots", sep = "/"))
-            dir.create(paste(input$dirLocation, data$sheet[row], "Voltage Profiles", sep = "/"))
-            dir.create(paste(input$dirLocation, data$sheet[row], "Voltage v Time", sep = "/"))
-            if (input$peakFit == "fit") {
-              dir.create(paste(input$dirLocation, data$sheet[row], "dQdV Peak Fitting", sep = "/"))
-            }
-          }
+          if (is.element("dQdV Graphs", input$gGraphs)) dir.create(paste(input$dirLocation, data$sheet[row], "dQdV Plots", sep = "/"))
+          if (is.element("Voltage Profiles", input$gGraphs)) dir.create(paste(input$dirLocation, data$sheet[row], "Voltage Profiles", sep = "/"))
+          if (is.element("Voltage vs. Time", input$gGraphs)) dir.create(paste(input$dirLocation, data$sheet[row], "Voltage v Time", sep = "/"))
+          if (input$peakFit == "fit") dir.create(paste(input$dirLocation, data$sheet[row], "dQdV Peak Fitting", sep = "/"))
           
           tmp_excel$Q.d <- as.numeric(tmp_excel$`Discharge_Capacity(Ah)` * (1000 / data$Mass[[1]][row]))
           tmp_excel$Q.c <- as.numeric(tmp_excel$`Charge_Capacity(Ah)`* (1000 / data$Mass[[1]][row]))
@@ -221,14 +218,18 @@ if (interactive()) {
             
             
             
-            if (ch_dch && input$gGraphs == "genGraphs") {
-              png(paste(input$dirLocation, "/", data$sheet[row], "/", "dQdV Plots/", data$name[row], data$sheet[row], "Cycle ", toString(i)," dQdV Plot.png", sep = ""))
-              plot(dQdVData[dQdVData$cycle == i,]$voltage, dQdVData[dQdVData$cycle == i,]$dQdV, main=paste("dQdV Plot for ",  input$dirLocation, data$sheet[row], "Cycle ", toString(i)), xlab="Voltage (V)", ylab="dQdV (mAh/V)")
-              dev.off()
-              
-              png(paste(input$dirLocation, "/", data$sheet[row], "/", "Voltage Profiles/", data$name[row], data$sheet[row], "Cycle ", toString(i)," Voltage Profile Plot.png", sep = ""))
-              plot(tmp_excel[tmp_excel$`Cycle_Index` == i,]$`Q.d`, tmp_excel[tmp_excel$`Cycle_Index` == i,]$`Voltage(V)`, type="l", main=paste("Voltage Profile for ",  input$dirLocation, data$sheet[row]), xlab="Discharge Capacity (mAh/g)", ylab="Voltage (V)")
-              dev.off()
+            if (ch_dch) {
+              if (is.element("dQdV Graphs", input$gGraphs)) {
+                png(paste(input$dirLocation, "/", data$sheet[row], "/", "dQdV Plots/", data$name[row], data$sheet[row], "Cycle ", toString(i)," dQdV Plot.png", sep = ""))
+                plot(dQdVData[dQdVData$cycle == i,]$voltage, dQdVData[dQdVData$cycle == i,]$dQdV, main=paste("dQdV Plot for ",  input$dirLocation, data$sheet[row], "Cycle ", toString(i)), xlab="Voltage (V)", ylab="dQdV (mAh/V)")
+                dev.off()
+              }
+                
+              if (is.element("Voltage Profiles", input$gGraphs)) {
+                png(paste(input$dirLocation, "/", data$sheet[row], "/", "Voltage Profiles/", data$name[row], data$sheet[row], "Cycle ", toString(i)," Voltage Profile Plot.png", sep = ""))
+                plot(tmp_excel[tmp_excel$`Cycle_Index` == i,]$`Q.d`, tmp_excel[tmp_excel$`Cycle_Index` == i,]$`Voltage(V)`, type="l", main=paste("Voltage Profile for ",  input$dirLocation, data$sheet[row]), xlab="Discharge Capacity (mAh/g)", ylab="Voltage (V)")
+                dev.off()
+              }
               
               if (input$peakFit == "fit") {
                 w = 20
@@ -253,7 +254,7 @@ if (interactive()) {
               }
             }
             
-            if (input$gGraphs == "genGraphs") {
+            if (is.element("Voltage vs. Time", input$gGraphs)) {
               png(paste(input$dirLocation, "/", data$sheet[row], "/", "Voltage v Time/", data$name[row], data$sheet[row], "Cycle ", toString(i)," Voltage Profile Plot.png", sep = ""))
               plot((tmp_excel[tmp_excel$`Cycle_Index` == i,]$`Test_Time(s)` - tmp_excel[tmp_excel$`Cycle_Index` == i,]$`Test_Time(s)`[[1]]) / 60, (tmp_excel[tmp_excel$`Cycle_Index` == i,]$`Voltage(V)` - tmp_excel[tmp_excel$`Cycle_Index` == i,]$`Voltage(V)`[[1]]) / 60, type="l", main=paste("Voltage vs. Time for ",  input$dirLocation, data$sheet[row]), xlab="Time (min)", ylab="Voltage (V)")
               dev.off()
@@ -267,24 +268,28 @@ if (interactive()) {
           
           meanDCap <- aggregate(tmp_excel$`Q.d`, by=list(tmp_excel$`Cycle_Index`), last)
           
-          if (input$gGraphs == "genGraphs") {
+          if (is.element("Discharge Capacity", input$gGraphs)) {
             png(paste(input$dirLocation, "/", data$sheet[row], "/", data$name[row], data$sheet[row]," Discharge Capacity Plot.png", sep = ""))
             eol <- meanDCap[1,2] * 0.8
             plot(meanDCap[,1], meanDCap[,2], main=paste("Discharge Capacity for ",  input$dirLocation, data$sheet[row]), xlab="Cycle", ylab="Discharge Capacity (mAh/g)")
             abline(h=eol, lty = "dotted")
             dev.off()
-            
+          }
+           
+          if (is.element("Discharge Areal Capacity", input$gGraphs)) {
             png(paste(input$dirLocation, "/", data$sheet[row], "/", data$name[row], data$sheet[row]," Discharge Areal Capacity Plot.png", sep = ""))
             eol <- ((meanDCap[1,2] * 1000) / data$Mass[[1]][row]) * 0.8
             plot(meanDCap[,1], ((meanDCap[,2] * 1000) / data$Mass[[1]][row]), main=paste("Discharge Capacity for ",  input$dirLocation, data$sheet[row]), xlab="Cycle", ylab="Discharge Capacity (mAh/cm^2)")
             abline(h=eol, lty = "dotted")
             dev.off()
+          }
             
+          if (is.element("dQdV Graphs", input$gGraphs)) {
             png_files <- list.files(paste(input$dirLocation, data$sheet[row], "dQdV Plots", sep="/"), pattern = "*.png", full.names = TRUE)
             gifski(png_files, gif_file = paste(input$dirLocation, data$sheet[row], "dQdV Plots", "dQdV All Cycles.gif", sep = "/"), delay = 0.1)
           }
 
-          if (input$gGraphs == "genGraphs") {
+         if (is.element("Average Voltage", input$gGraphs)) {
             png(paste(input$dirLocation, "/", data$sheet[row], "/", data$name[row], data$sheet[row]," Average Voltage Plot.png", sep = ""))
             plot(cycle_facts$cycle, cycle_facts$chV, col="blue", main=paste("dQdV Plot for ",  input$dirLocation, data$sheet[row]), xlab="Cycle", ylab="Voltage (V)", ylim=c(min(cycle_facts[,2:4]), max(cycle_facts[,2:4])))
             points(cycle_facts$cycle, cycle_facts$dchV, col="red", main=paste("dQdV Plot for ",  input$dirLocation, data$sheet[row]), xlab="Cycle", ylab="Voltage (V)")
@@ -310,23 +315,34 @@ if (interactive()) {
         stats <- select(stats, Mean.Discharge.Capacity.x, Mean.Charge.Capacity.x, St..Error.Discharge.Capacity.x, St..Error.Charge.Capacity.x)
         stats$Cycle <- 1:length(stats[,1])
 
-        png(paste(getwd(),"/", input$dirLocation, "/", data$name[row], "Total Discharge Capacity Plot.png", sep = ""))
-        eol <- stats[1,1] * 0.8
-        plot(stats$Cycle, stats$`Mean.Discharge.Capacity.x`, main=paste("Discharge Capacity for ",  input$dirLocation), xlab="Cycle", ylab="Discharge Capacity (mAh/g)")
-        # arrows(stats$Cycle, stats[,1] - stats[,3], stats$Cycle, stats[,1] + stats[,3], length=0.05, angle=90, code=3)
-        abline(h=eol, lty = "dotted")
-        dev.off()
+        if (is.element("Total Discharge Capacity", input$gGraphs)) {
+          png(paste(getwd(),"/", input$dirLocation, "/", data$name[row], "Total Discharge Capacity Plot.png", sep = ""))
+          eol <- stats[1,1] * 0.8
+          plot(stats$Cycle, stats$`Mean.Discharge.Capacity.x`, main=paste("Discharge Capacity for ",  input$dirLocation), xlab="Cycle", ylab="Discharge Capacity (mAh/g)")
+          # arrows(stats$Cycle, stats[,1] - stats[,3], stats$Cycle, stats[,1] + stats[,3], length=0.05, angle=90, code=3)
+          abline(h=eol, lty = "dotted")
+          dev.off()
+        }
         
         write.csv(stats, file = paste(getwd(),"/", input$dirLocation, "/", data$name[row], " Summary.csv", sep = ""))
         write.csv(final, file = paste(getwd(),"/", input$dirLocation, "/", data$name[row], " Total.csv", sep = ""))
         
-        # save(data, file = paste("history/", input$dirLocation, ".RData"))
+        save(data, file = paste("history/", input$dirLocation, ".RData"))
         
-        shinyalert("Analysis Complete!", paste("All your data is now in ", input$dirLocation), "success")
+        shinyalert("Analysis Complete!", paste("All your data are now in ", input$dirLocation), "success")
         
         remove(list=ls())
         
         incProgress((nrow(data)+ 1)/(nrow(data)+ 1))
+        
+        enable("files")
+        enable("lowV")
+        enable("highV")
+        enable("dirLocation")
+        enable("submit")
+        enable("excelImport")
+        enable("gGraphs")
+        enable("peakFit")
       })
     }
     
